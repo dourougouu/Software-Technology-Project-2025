e3232943836edf2b231c91bf3c5a950513d1700d
Mary Drg <164538178+dourougouu@users.noreply.github.com>
2025-05-22 02:42:34 +0300

    Create Home.java

---
+++ 4ο Παραδοτέο/Codes/Maria/Home.java
@@ -0,0 +1,9 @@
+//στην κλαση Home.java του Κωστα στο button για το Search Transportation προσθετουμε και αυτο
+
+private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // TODO add your handling code here:
+            SearchTransportation transportFrame = new SearchTransportation();
+            transportFrame.setVisible(true);
+            dispose();
+    }                                        
+ 
