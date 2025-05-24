c2b5f3114637fc7b48c471110ab046bde9212dd5
StrCha <stratosxatzidoukas@gmail.com>
2025-05-24 14:06:51 +0300

    Create UpdateAccommodationScreen.java

---
+++ 4ο Παραδοτέο/Codes/FinalClassesCode/UpdateAccommodationScreen.java
@@ -0,0 +1,13 @@
+public class UpdateAccommodationScreen {
+    private SiteManagement siteManagement;
+
+    public UpdateAccommodationScreen(SiteManagement siteManagement) {
+        this.siteManagement = siteManagement;
+    }
+
+    public void display() {
+        System.out.println("=== Update Accommodation Screen ===");
+        // Εμφάνιση επιλογών για ανανέωση/επεξεργασία καταλυμάτων
+    }
+}
+
