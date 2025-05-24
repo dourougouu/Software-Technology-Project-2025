027d9e5064f9003169835ede05d8851d48cf6e8b
KostasBex04 <124631100+KostasBex04@users.noreply.github.com>
2025-05-24 19:57:39 +0300

    Update DBManager.java

--- 4ο Παραδοτέο/Codes/FinalClassesCode/DBManager.java
+++ 4ο Παραδοτέο/Codes/FinalClassesCode/DBManager.java
@@ -1,1 +1,38 @@
+import java.util.List;
+
+public class DBManager {
+    public boolean ckeckIfUserExists(){
+
+    }
+    public boolean saveUser(){
+
+    }
+    public boolean queryConcertTickets(){
+
+    }
+    public boolean queryTransportationTickets(){
+
+    }
+    public boolean queryAccommodation(){
+
+    }
+    public boolean queryUCon(){
+
+    }
+    public boolean queryUTransp(){
+
+    }
+    public boolean queryUAc(){
+
+    }
+    public boolean updateAc(){
+
+    }
+    public boolean updateCon(){
+
+    }
+    public boolean updateTransp(){
+        
+    }
+}
 
