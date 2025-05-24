d21bdfce14fcc7490d08dd2f7f4a67a7a5d7d4b0
StrCha <stratosxatzidoukas@gmail.com>
2025-05-24 14:06:17 +0300

    Create UpdateConcertScreen.java

---
+++ 4ο Παραδοτέο/Codes/FinalClassesCode/UpdateConcertScreen.java
@@ -0,0 +1,12 @@
+public class UpdateConcertScreen {
+    private SiteManagement siteManagement;
+
+    public UpdateConcertScreen(SiteManagement siteManagement) {
+        this.siteManagement = siteManagement;
+    }
+
+    public void display() {
+        System.out.println("=== Update Concert Screen ===");
+        
+    }
+}
