adf21a981118698dc2fa8bba9ad7c843cd2a46d3
StrCha <stratosxatzidoukas@gmail.com>
2025-05-24 14:05:53 +0300

    Create UpdateTransportationScreen.java

---
+++ 4ο Παραδοτέο/Codes/FinalClassesCode/UpdateTransportationScreen.java
@@ -0,0 +1,11 @@
+public class UpdateTransportationScreen {
+    private SiteManagement siteManagement;
+
+    public UpdateTransportationScreen(SiteManagement siteManagement) {
+        this.siteManagement = siteManagement;
+    }
+
+    public void display() {
+        System.out.println("=== Update Transportation Screen ===");
+            }
+}
