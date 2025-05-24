0a82cc33f33ac43beca6ddf7a418995f5957bc31
KostasBex04 <124631100+KostasBex04@users.noreply.github.com>
2025-05-24 19:58:34 +0300

    Create UserMessagesScreen.java

---
+++ 4ο Παραδοτέο/Codes/FinalClassesCode/UserMessagesScreen.java
@@ -0,0 +1,18 @@
+public class UserMessagesScreen {
+    private Message message;
+
+    public void selectMessage() {
+        // Logic for selecting a message
+        System.out.println("Message selected: " + message);
+    }
+
+    public void spam() {
+        // Logic to mark message as spam
+        System.out.println("Message marked as spam: " + message);
+    }
+
+    public void displayMessage() {
+        // Logic to display message
+        System.out.println("Displaying message: " + message);
+    }
+}
