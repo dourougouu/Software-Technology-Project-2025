fdd1415769af28a08cde2b45af08647028d7e64e
KostasBex04 <124631100+KostasBex04@users.noreply.github.com>
2025-05-24 19:57:55 +0300

    Create ManageMessageClass.java

---
+++ 4ο Παραδοτέο/Codes/FinalClassesCode/ManageMessageClass.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class ManageMessageClass {
+    private Message message;
+
+    public void submitMessage() {
+        // Logic to submit the message
+        System.out.println("Message submitted: " + message);
+    }
+
+    public void startUpdate() {
+        // Logic to start message update
+        System.out.println("Updating message: " + message);
+    }
+}
